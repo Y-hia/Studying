3a32e257cfcc32dc55d8523f54de4524acc418c6
PC <2352367175@qq.com>
2025-02-26 18:43:31 +0800

    1

--- Study/src/main/java/com/study/Array/Login.java
+++ Study/src/main/java/com/study/Array/Login.java
@@ -2,7 +2,7 @@ package com.study.Array;
 
 public class Login {
     public static void main(String[] args) {
-        Captcha.printCaptcha(args);
+        System.out.println(Captcha.printCaptcha(args));
         System.out.println(Captcha.roudm);
     }
 }
