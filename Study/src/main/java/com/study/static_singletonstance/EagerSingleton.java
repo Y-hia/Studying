291b68a18ea093d3f543eca40fe3ca355c11cc3f
PC <2352367175@qq.com>
2025-02-27 16:50:23 +0800

    2

---
+++ Study/src/main/java/com/study/static_singletonstance/EagerSingleton.java
@@ -0,0 +1,15 @@
+package com.study.static_singletonstance;
+
+public class EagerSingleton {
+
+    public static EagerSingleton name;
+
+    private EagerSingleton(){
+
+    }
+
+
+    public static EagerSingleton getName(){
+        return  name;
+    }
+}
