0fe8581004e1629e276e9002089036646ce2f772
PC <2352367175@qq.com>
2025-02-27 10:04:56 +0800

    1

--- Study/src/main/java/com/study/Array/Captcha.java
+++ Study/src/main/java/com/study/Array/Captcha.java
@@ -1,13 +1,37 @@
 package com.study.Array;
 
+import java.util.Random;
+
 public class Captcha {
-
-    public static int roudm;
-
+    /**
+     * 验证码
+     * @param args
+     * @return 6位数的验证码
+     */
     public static String printCaptcha(String[] args) {
-        String number = String.valueOf((int) (Math.random() * 10000));
-        roudm ++;
-        return number;
+        Random random = new Random();
+        StringBuilder number = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+           int num = random.nextInt(10);
+           number.append(num);
+        }
+        return number.toString();
     }
+
+    public static String generatorCaptcha(String[] args) {
+
+        String charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder captcha = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(charSet.length());
+            captcha.append(charSet.charAt(index));
+        }
+
+        return captcha.toString();
+    }
+
+
+
 }
 
--- Study/src/main/java/com/study/Array/Login.java
+++ Study/src/main/java/com/study/Array/Login.java
@@ -2,7 +2,8 @@ package com.study.Array;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println(Captcha.printCaptcha(args));
-        System.out.println(Captcha.roudm);
+
+        System.out.println("六位纯数字的验证码"+Captcha.printCaptcha(args));
+        System.out.println("六位包含英文和数字的验证码"+Captcha.generatorCaptcha(args));
     }
 }
