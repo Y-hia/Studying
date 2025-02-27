6ae770708070195eea7945eb86ee8aaf9f7b976f
PC <2352367175@qq.com>
2025-02-27 10:12:15 +0800

    1

--- Study/src/main/java/com/study/Array/Captcha.java
+++ Study/src/main/java/com/study/Array/Captcha.java
@@ -4,6 +4,19 @@ import java.util.Random;
 
 public class Captcha {
     /**
+     * 生成包含数字和英文的验证码
+     */
+    private static final String charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    public static String generatorCaptcha(String[] args) {
+        StringBuilder captcha = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(charSet.length());
+            captcha.append(charSet.charAt(index));
+        }
+        return captcha.toString();
+    }
+    /**
      * 验证码
      * @param args
      * @return 6位数的验证码
@@ -12,26 +25,11 @@ public class Captcha {
         Random random = new Random();
         StringBuilder number = new StringBuilder();
         for (int i = 0; i < 6; i++) {
-           int num = random.nextInt(10);
-           number.append(num);
+            int num = random.nextInt(10);
+            number.append(num);
         }
         return number.toString();
     }
-
-    public static String generatorCaptcha(String[] args) {
-
-        String charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        StringBuilder captcha = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < 6; i++) {
-            int index = random.nextInt(charSet.length());
-            captcha.append(charSet.charAt(index));
-        }
-
-        return captcha.toString();
-    }
-
-
 
 }
 
