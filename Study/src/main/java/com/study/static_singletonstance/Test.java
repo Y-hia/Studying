9a364922ae42bffa2bc4e2f01216cf901a3b9f11
PC <2352367175@qq.com>
2025-02-27 16:50:23 +0800

    tijiao

---
+++ Study/src/main/java/com/study/static_singletonstance/Test.java
@@ -0,0 +1,10 @@
+package com.study.static_singletonstance;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(EagerSingleton.getName());
+    }
+
+}
