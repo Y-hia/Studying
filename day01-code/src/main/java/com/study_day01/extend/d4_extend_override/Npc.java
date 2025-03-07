4d5ea67914816ba1b3dab19b26161c8eb097b9db
PC <2352367175@qq.com>
2025-03-07 10:07:07 +0800

    Merge remote-tracking branch 'origin/main'

--- day01-code/src/main/java/com/study_day01/extend/d4_extend_override/Npc.java
+++ day01-code/src/main/java/com/study_day01/extend/d4_extend_override/Npc.java
@@ -1,6 +1,8 @@
 package com.study_day01.extend.d4_extend_override;
 
-public class Npc extends Person{
+import com.study_day01.extend.d4_extend_override.Person;
+
+public class Npc extends Person {
     @Override
     public void eat() {
         System.out.println("Npc会吃更多的饭");
--- day01-code/src/main/java/com/study_day01/static_singletonstance/Test.java
+++ day01-code/src/main/java/com/study_day01/static_singletonstance/Test.java
@@ -1,5 +1,7 @@
 package com.study_day01.static_singletonstance;
 
+import com.study_day01.static_singletonstance.EagerSingleton;
+
 public class Test {
 
     public static void main(String[] args) {
