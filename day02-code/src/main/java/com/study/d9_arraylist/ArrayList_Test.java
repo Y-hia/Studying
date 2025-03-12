96a620140e9515354c0964038d641d7f8c97a574
Y-hia <2352367175@qq.com>
2025-03-12 23:59:34 +0800

    模板设计方法的学习

--- day02-code/src/main/java/com/study/d9_arraylist/ArrayList_Test.java
+++ day02-code/src/main/java/com/study/d9_arraylist/ArrayList_Test.java
@@ -7,9 +7,28 @@ public class ArrayList_Test {
         ArrayList<String> list = new ArrayList<>();
         //1、添加元素
         list.add("张三");
+        list.add("李四");
+        list.add("王五");
         System.out.println(list);
+
         //2、指定下标添加元素
-        list.addFirst("李四");
+        list.add(1,"赵六");
         System.out.println(list);
+
+        //3、删除元素，并返回真假，默认删除第一个符合条件的元素
+        System.out.println(list.remove("张三"));
+        System.out.println(list);
+
+        //4、删除指定索引下的元素，并返回被删除的元素
+        System.out.println(list.remove(1));
+        System.out.println(list);
+
+        //5、获取指定索引的元素
+        System.out.println(list.get(1));
+
+        //6、修改指定索引的元素
+        list.set(1,"丁麻子");
+        System.out.println(list);
+
     }
 }
---
+++ day02-code/src/main/java/com/study/d9_arraylist/ArrayList_demo1.java
@@ -0,0 +1,31 @@
+package com.study.d9_arraylist;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ArrayList_demo1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("ABC");
+        list.add("BCD");
+        list.add("CDE");
+        list.add("DEF");
+        list.add("EFG");
+
+        for (int i = 0; i < list.size(); i++) {
+            if (Objects.equals(list.get(i), "ABC")){
+                list.remove(i);
+                i--;
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            //判断集合中的元素是否包含C
+            if(list.get(i).contains("C")){
+                list.remove(i);
+                i--;
+            };
+        }
+        System.out.println(list);
+    }
+}
