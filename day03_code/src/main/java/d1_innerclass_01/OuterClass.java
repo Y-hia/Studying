23a4d95355ecea80f1217ca37908ffe29627d1f2
PC <2352367175@qq.com>
2025-03-13 16:43:08 +0800

    成员内部类完善
    Signed-off-by: PC <2352367175@qq.com>

--- day03_code/src/main/java/d1_innerclass_01/OuterClass.java
+++ day03_code/src/main/java/d1_innerclass_01/OuterClass.java
@@ -1,13 +1,28 @@
 package d1_innerclass_01;
 
 public class OuterClass {
-    //外部类私有成员
-    private String name;
+    //外部类静态变量
+    private static String dog = "狗子";
+    //外部类静态方法
+    public static void go(){
+        System.out.println("开始跑");
+    }
 
+    //外部类的实例变量
+    String bord = "小鸟";
+    //外部类实例方法
+    public void fly(){
+        System.out.println("开始飞");
+    }
     //成员内部类
     public class InnerClass{
-        void show(){
-
+        public void run(){
+            //内部类可以直接访问外部类的静态方法和静态变量
+            go();
+            System.out.println(dog+"跑的很快~~~~");
+            //内部类可以直接访问外部类的实例方法和实例变量
+            fly();
+            System.out.println(bord+"飞的很高~~~~");
         }
 
     }
