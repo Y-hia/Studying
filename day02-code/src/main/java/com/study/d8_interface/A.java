1aa4e6d9afb0af73d2fa80628bf6f7d0dba52e91
PC <2352367175@qq.com>
2025-03-13 15:53:43 +0800

    接口的特点
    Signed-off-by: PC <2352367175@qq.com>

--- day02-code/src/main/java/com/study/d8_interface/A.java
+++ day02-code/src/main/java/com/study/d8_interface/A.java
@@ -2,7 +2,7 @@ package com.study.d8_interface;
 
 /**
  * 接口：1.8之前的接口，只能定义常量和抽象方法
- * 接口不能创建的对象，接口都是用来被实现（implements）的，实现接口的类一半都称之为 实现类
+ * 接口不能创建的对象，接口都是用来被实现（implements）的，实现接口的类一般都称之为 实现类
  */
 public interface A {
     //public static final String NAME=  null ;
--- day02-code/src/main/java/com/study/d8_interface/B.java
+++ day02-code/src/main/java/com/study/d8_interface/B.java
@@ -1,6 +1,24 @@
 package com.study.d8_interface;
 
+/**
+ * JDK8 以后接口新增了三种方法
+ */
 public interface B{
     void run();
+
+    //默认方法，
+    default void look() {
+
+        test();
+        System.out.println("吃东西");
+    }
+    //静态方法
+    static void go() {
+        System.out.println("去 somewhere");
+    }
+    //私有方法
+    private void test() {
+        System.out.println("test");
+    }
 }
 
