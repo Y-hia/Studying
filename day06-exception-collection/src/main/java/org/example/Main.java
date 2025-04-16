6824ec20627ed094e9ce00e1e160d838dfb6bf86
PC <2352367175@qq.com>
2025-04-16 14:48:33 +0800

    1

--- day06-exception-collection/src/main/java/org/example/Main.java
+++ day06-exception-collection/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Solution {
+
+    /* Write Code Here */
+    public int isEvenNum(int n) {
+        return n % 2 == 0? 1 : -1;
+    }
+}
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int res;
+
+        int n;
+        n = Integer.parseInt(in.nextLine().trim());
+        res = new Solution().isEvenNum(n);
+        System.out.println(String.valueOf(res));
+
     }
 }
